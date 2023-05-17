da107832df2f65dd3f904b581edf656ce66e9403
codehashira28 <123044813+codehashira28@users.noreply.github.com>
2023-05-16 23:45:31 -0500

    added functionality to dynamically color-code time blocks to indicate whether they are in the past, present, or future based on current time

--- script.js
+++ script.js
@@ -18,6 +18,22 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+
+  var currentHour = Number(dayjs().format('HH'));
+  console.log(currentHour);
+  var timeblocks = $('.time-block');
+
+  for(slot of timeblocks) {
+    var entry = $('#' + slot.id);
+    if(currentHour < slot.id.split('-')[1]) {
+      entry.addClass('future');
+    } else if(currentHour > slot.id.split('-')[1]) {
+      entry.addClass('past');
+    } else {
+      entry.addClass('present');
+    }
+  }
+  ;
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
